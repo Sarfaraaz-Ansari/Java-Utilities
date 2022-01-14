d53a149f729f14f3d307005de7441631bec458b2
Sarfaraaz-Ansari <46718511+Sarfaraaz-Ansari@users.noreply.github.com>
2022-01-15 01:44:11 +0530

    Add files via upload

---
+++ src/test/java/com/ansasar/alphabetify/AlphabetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansasar.alphabetify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabetifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
