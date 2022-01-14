ca19c96b86c929be428de5173fbc49dd0a3ca212
Sarfaraaz-Ansari <46718511+Sarfaraaz-Ansari@users.noreply.github.com>
2022-01-15 01:40:23 +0530

    Add files via upload

---
+++ src/main/java/com/ansasar/alphabetify/AlphabetifyApplication.java
@@ -0,0 +1,13 @@
+package com.ansasar.alphabetify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabetifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphabetifyApplication.class, args);
+	}
+
+}
